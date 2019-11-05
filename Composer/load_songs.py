6b25b818f2805175807de4d35b11b2205ad1ca3f
Kinbert Chou <kinbertchou@gmail.com>
2019-11-04 21:49:33 -0800

    load_songs

--- Composer/load_songs.py
+++ Composer/load_songs.py
@@ -1,13 +1,25 @@
+
+""" 
+load_songs.py: Loads and saves midi files to loadable data. 
+
+Usage:
+    load_songs.py --load_path=<path> --save_path=<path>
+
+Options:
+    -h --help                               show this screen.
+	-l --load_path=<path> 					path to metadata CSV 
+	-s --save_path=<path> 					path to directory to hold processed data 
+"""
 import midi
-import os
+import os, sys 
 import util
-import numpy as np
 import argparse 
 import json 
 import pandas as pd 
+import numpy as np
+from docopt import docopt
 
-
-def load_metadata(filepath: str):
+def load_metadata(filename: str):
 	''' 
 	@param filepath str: path to CSV file containing the metadata table for the Magenta Maestro dataset. 
 	
@@ -20,10 +32,7 @@ def load_metadata(filepath: str):
 	
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('l', '--load_path', help='Filepath to metadata CSV')
-	parser.add_argument('s', '--save_path', help='Path to directory holding processed data')
-	args = parser.parse_args()
+	args = docopt(__doc__)
 
 	patterns = {}
 	all_samples = []
@@ -32,6 +41,16 @@ def main():
 
 	metadata = load_metadata(args['--load_path'])
 	data_dir, _ = os.path.split(args['--load_path'])
+
+	splits = ['train', 'validation', 'test']
+
+	for category in splits:
+		directory = os.path.join(args['--save_path'], category)
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+		
+		curr_set = metadata[metadata['split'] == category]
+		print(curr_set.head())
 				# if not (path.endswith('.mid') or path.endswith('.midi')):
 				# 	continue
 				# try:
@@ -46,13 +65,13 @@ def main():
 				# all_samples += samples
 				# all_lens += lens
 		
-	assert(sum(all_lens) == len(all_samples))
-	print('Saving ' + str(len(all_samples)) + ' samples...')
-	all_samples = np.array(all_samples, dtype=np.uint8)
-	all_lens = np.array(all_lens, dtype=np.uint32)
-	np.save('samples.npy', all_samples)
-	np.save('lengths.npy', all_lens)
-	print('Done')
+	# assert(sum(all_lens) == len(all_samples))
+	# print('Saving ' + str(len(all_samples)) + ' samples...')
+	# all_samples = np.array(all_samples, dtype=np.uint8)
+	# all_lens = np.array(all_lens, dtype=np.uint32)
+	# np.save('samples.npy', all_samples)
+	# np.save('lengths.npy', all_lens)
+	# print('Done')
 
-if __name__ == '__main__': 
-	main() 
+if __name__ == '__main__':
+    main()
